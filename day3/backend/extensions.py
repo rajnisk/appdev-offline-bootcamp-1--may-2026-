1db54acb002fccbab6c2568c75f8170e7d95daac
Rajnish <22f2000625@ds.study.iitm.ac.in>
2026-06-18 15:26:32 +0530

    Remove frontend files and components for the task manager application

--- day3/backend/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_caching import Cache
-from flask_mail import Mail
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-mail = Mail()
-cache = Cache()
--- day3/frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
